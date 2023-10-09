b91ea85460e014c254642ae6d30ffed6559b3903
AnujKrSharma <anujkrsharma@hotmail.com>
2023-10-09 21:33:12 +0530

    adding html file

---
+++ pet-clinic-web/src/main/java/com/olive/springfwrkpetclinic/Controllers/OwnerController.java
@@ -0,0 +1,22 @@
+package com.olive.springfwrkpetclinic.Controllers;
+
+import org.olive.springfwrkpetclinic.service.OwnerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/Owners")
+@Controller
+public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwners(Model model){
+
+        model.addAttribute("owners",ownerService.findall());
+        return "owners/index";
+    }
+}
